a0bcc66aa5a9f6d1c4dc62e9c9014105a44717ca
David V. Lu <davidvlu@gmail.com>
2022-09-23 12:38:44 -0400

    Use InterfaceInterface for base commands

--- ros_command/commands/rosinterface.py
+++ ros_command/commands/rosinterface.py
@@ -10,12 +10,12 @@ from ros_command.workspace import get_ros_version
 ACTION_PARTS = ['Goal', 'Result', 'Feedback']
 
 
-async def list_actions(distro, pkg=None):
+async def list_actions(ii, distro, pkg=None):
     if pkg:
         packages = [pkg]
     else:
         # List all packages with any messages
-        _, out, _ = await get_output([f'/opt/ros/{distro}/bin/rosmsg', 'packages'])
+        _, out, _ = await get_output(ii.get_base_command('packages', interface_type='msg'))
         packages = set(filter(None, out.split('\n')))
 
     results = []
@@ -28,7 +28,7 @@ async def list_actions(distro, pkg=None):
     return sorted(results)
 
 
-async def list_interfaces_ros2(distro, types=None):
+async def list_interfaces_ros2(ii, types=None):
     interfaces = []
 
     def out_cb(line):
@@ -38,12 +38,12 @@ async def list_interfaces_ros2(distro, types=None):
             if types is not None and pieces[1] in types:
                 interfaces.append(line)
 
-    await run([f'/opt/ros/{distro}/bin/ros2', 'interface', 'list'], stdout_callback=out_cb)
+    await run(ii.get_base_command('list'), stdout_callback=out_cb)
     return interfaces
 
 
-async def list_interfaces_by_name_ros2(distro, types=None):
-    interfaces = await list_interfaces_ros2(distro, types)
+async def list_interfaces_by_name_ros2(ii, types=None):
+    interfaces = await list_interfaces_ros2(ii, types)
 
     stems = collections.defaultdict(list)
     for interface in interfaces:
@@ -53,7 +53,7 @@ async def list_interfaces_by_name_ros2(distro, types=None):
     return stems
 
 
-async def translate_to_full_names(base_s, interface_type, distro):
+async def translate_to_full_names(base_s, interface_type, ii):
     pieces = base_s.split('/')
     if len(pieces) == 3:
         # String already is full qualified
@@ -63,8 +63,24 @@ async def translate_to_full_names(base_s, interface_type, distro):
         return [f'{pieces[0]}/{interface_type}/{pieces[1]}']
     else:
         # If only the interface name is specified, list all the interfaces and return the matches
-        stems = await list_interfaces_by_name_ros2(distro, [interface_type])
+        stems = await list_interfaces_by_name_ros2(ii, [interface_type])
         return stems[base_s]
+
+
+class InterfaceInterface:
+    def __init__(self, version, distro, interface_type):
+        self.version = version
+        self.distro = distro
+        self.interface_type = interface_type
+
+    def get_base_command(self, verb, interface_type=None):
+        if interface_type is None:
+            interface_type = self.interface_type
+        if self.version == 1:
+            return [f'/opt/ros/{self.distro}/bin/ros{interface_type}', verb]
+        else:
+            cmd = [f'/opt/ros/{self.distro}/bin/ros2', 'interface', verb]
+            return cmd
 
 
 async def main(interface_type):
@@ -85,10 +101,12 @@ async def main(interface_type):
 
     version, distro = get_ros_version()
 
+    ii = InterfaceInterface(version, distro, interface_type)
+
     if version == 1:
         if interface_type != 'action':
             # Pass through for rosmsg/rossrv
-            cmd = [f'/opt/ros/{distro}/bin/ros{interface_type}', args.verb]
+            cmd = ii.get_base_command(args.verb)
             if args.verb in ['show', 'md5']:
                 cmd.append(args.interface_name)
             elif args.verb == 'package':
@@ -97,28 +115,30 @@ async def main(interface_type):
             exit(code)
         elif args.verb in ['show', 'md5']:
             for part in ACTION_PARTS:
-                await run([f'/opt/ros/{distro}/bin/rosmsg', args.verb, f'{args.interface_name}{part}'])
+                cmd = ii.get_base_command(args.verb)
+                cmd.append(f'{args.interface_name}{part}')
+                await run(cmd)
         elif args.verb == 'list':
-            for pkg, action in await list_actions(distro):
+            for pkg, action in await list_actions(ii, distro):
                 print(f'{pkg}/{action}')
         elif args.verb == 'package':
-            for pkg, action in await list_actions(distro, pkg=args.package_name):
+            for pkg, action in await list_actions(ii, distro, pkg=args.package_name):
                 print(f'{pkg}/{action}')
         elif args.verb == 'packages':
             seen = set()
-            for pkg, action in await list_actions(distro):
+            for pkg, action in await list_actions(ii, distro):
                 if pkg not in seen:
                     print(pkg)
                     seen.add(pkg)
 
     elif args.verb == 'show':
-        base_command = [f'/opt/ros/{distro}/bin/ros2', 'interface', 'show']
-        for full_name in await translate_to_full_names(args.interface_name, interface_type, distro):
+        base_command = ii.get_base_command(args.verb)
+        for full_name in await translate_to_full_names(args.interface_name, interface_type, ii):
             click.secho(f'[{full_name}]', fg='blue')
             await run(base_command + [full_name])
     elif args.verb in ['list', 'packages']:
         # Pass through to ros2 interface with appropriate args
-        command = [f'/opt/ros/{distro}/bin/ros2', 'interface', args.verb]
+        command = ii.get_base_command(args.verb)
         command.append('-' + interface_type[0])  # -m for msg, -s for srv, etc
         await run(command)
     elif args.verb == 'package':
@@ -128,7 +148,7 @@ async def main(interface_type):
         def package_output_cb(line):
             if key in line:
                 click.secho(line, nl=False)
-        command = [f'/opt/ros/{distro}/bin/ros2', 'interface', 'package']
+        command = ii.get_base_command(args.verb)
         await run(command + [args.package_name], stdout_callback=package_output_cb)
     else:
         raise NotImplementedError('No equivalent md5 command in ros2')
