b86fd95bea3107ddaff46fdb44025b68ec7d31fb
David V. Lu <davidvlu@gmail.com>
2023-02-24 09:55:14 -0500

    rosrun completion

--- ros_command/commands/rosrun.py
+++ ros_command/commands/rosrun.py
@@ -2,16 +2,22 @@ import argcomplete
 import argparse
 
 from ros_command.command_lib import run
-from ros_command.workspace import get_ros_version
+from ros_command.completion import PackageCompleter, ExecutableNameCompleter
+from ros_command.workspace import get_ros_version, get_workspace_root
 
 
 async def main(debug=False):
+    build_type, workspace_root = get_workspace_root()
+    version, distro = get_ros_version()
+
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--debug', action='store_true')
-    argcomplete.autocomplete(parser)
-    args, unknown_args = parser.parse_known_args()
+    parser.add_argument('package_name').completer = PackageCompleter(workspace_root)
+    parser.add_argument('executable_name').completer = ExecutableNameCompleter(version)
+    parser.add_argument('argv', nargs=argparse.REMAINDER)
 
-    version, distro = get_ros_version()
+    argcomplete.autocomplete(parser, always_complete_options=False)
+    args = parser.parse_args()
 
     command = []
     if version == 1:
@@ -20,8 +26,12 @@ async def main(debug=False):
         command.append(f'/opt/ros/{distro}/bin/ros2')
         command.append('run')
 
+    command += [args.package_name, args.executable_name]
+
     if debug or args.debug:
         command += ['--prefix', 'gdb -ex run --args']
 
-    code = await run(command + unknown_args)
+    command += args.argv
+
+    code = await run(command)
     exit(code)
--- ros_command/completion.py
+++ ros_command/completion.py
@@ -1,4 +1,5 @@
 from ros_command.packages import get_all_packages, get_packages_in_folder
+from ros_command.packages import find_executables_in_package
 
 
 class PackageCompleter:
@@ -11,3 +12,11 @@ class PackageCompleter:
             return get_packages_in_folder(self.workspace_root)
         else:
             return get_all_packages(self.workspace_root)
+
+
+class ExecutableNameCompleter:
+    def __init__(self, version):
+        self.version = version
+
+    def __call__(self, parsed_args, **kwargs):
+        return find_executables_in_package(parsed_args.package_name, self.version)
