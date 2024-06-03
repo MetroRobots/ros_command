43230d7e3025981c2f704a5d31bda5de08eea89c
David V. Lu <davidvlu@gmail.com>
2024-06-03 16:57:41 -0400

    Remove unused import

--- src/ros_command/commands/rosexecute.py
+++ src/ros_command/commands/rosexecute.py
@@ -6,7 +6,7 @@ from betsy_ros import get_ros_version, get_workspace_root
 
 from ros_command.command_lib import run, get_overlayed_command
 from ros_command.completion import PackageCompleter, ExecutableNameCompleter, LaunchFileCompleter
-from ros_command.packages import find_executables_in_package, find_launch_files_in_package
+from ros_command.packages import find_executables_in_package
 
 
 class StartCompleter:
